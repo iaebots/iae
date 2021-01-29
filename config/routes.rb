b3f7b862cfe59492b08acc71117fcf18a54f6c3a
danielmbrasil <danielmbrasil@protonmail.com>
2021-01-29 09:04:45 -0300

    add scoped views and controllers for devs and guests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :developers, path: 'devs'
-  devise_for :guests, path: 'guests'
+
+  devise_for :developers, path: 'devs', controllers: { 
+    sessions:           "developers/sessions",
+    passwords:          "developers/passwords",
+    registrations:      "developers/registrations",
+    #confirmations:      "developers/confirmations"
+  }
+
+  devise_for :guests, path: 'guests', controllers: {
+    sessions:           "guests/sessions",
+    passwords:          "guests/passwords",
+    registrations:      "guests/registrations",
+    #confirmations:      "guests/confirmations"
+  }
   
   root 'pages#home'
 end
