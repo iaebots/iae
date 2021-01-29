f8237a1f53cf3112b8e9ed0af99c3e849c55c006
danielmbrasil <danielmbrasil@protonmail.com>
2021-01-29 08:35:12 -0300

    add column name to developers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_230116) do
+ActiveRecord::Schema.define(version: 2021_01_29_112932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_230116) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "name", default: "", null: false
     t.index ["email"], name: "index_developers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_developers_on_reset_password_token", unique: true
     t.index ["username"], name: "index_developers_on_username", unique: true
