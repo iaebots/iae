4f3faa2282295cdcc83b1cb2deafd1ee1e3a600c
danielmbrasil <danielmbrasil@protonmail.com>
2021-02-18 09:39:40 -0300

    add relation between bot and developer

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,4 +1,6 @@
 class Bot < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  belong_to: :developer
 end
--- app/models/developer.rb
+++ app/models/developer.rb
@@ -11,14 +11,16 @@ class Developer < ApplicationRecord
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   before_save :downcase_username
 
+  has_many: :bots, dependent: :destroy
+  
   # evita letras maisúculas em nomes de usuários
   def downcase_username
     self.username.downcase!
   end
-  
+
   def login
-    @login or self.username or self.email     
-  end  
+    @login or self.username or self.email
+  end
 
   # override do método de autenticação para permitir login com outro parâmetro
   def self.find_for_database_authentication(warden_conditions)
@@ -27,7 +29,7 @@ class Developer < ApplicationRecord
       where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value ", {:value => login.downcase}]).first
     elsif conditions.has_key?(:username) or conditions.has_key?(:email)
       where(conditions.to_h).first
-    end 
+    end
   end
 
   # valida se o nome de usuário não está sendo utilizado
@@ -35,7 +37,7 @@ class Developer < ApplicationRecord
     if Guest.where(email: username.downcase).exists? or Developer.where(email: username.downcase).exists?
       errors.add(:username, :invalid)
     elsif Guest.where(username: username.downcase).exists? or Developer.where(username: username.downcase).exists?
-      errors.add(:username, :invalid) 
+      errors.add(:username, :invalid)
     end
   end
 end
