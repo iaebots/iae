274e9af8b059a3c7235c135cf017f47583000809
danielmbrasil <danielmbrasil@protonmail.com>
2021-03-05 22:20:26 -0300

    autocomplete now only queries tags that looks like users input

--- app/controllers/autocompletes_controller.rb
+++ app/controllers/autocompletes_controller.rb
@@ -1,7 +1,21 @@
 class AutocompletesController < ApplicationController
+  before_action :query_params, only: %i[show]
+
+  # Returns a JSON with the name of tags that looks like users' input
   def show
-    response = Tag.pluck(:name).sort
+   response = Tag.find_by_sql("
+     SELECT name
+     FROM Tags
+     WHERE name LIKE '%#{params[:query]}%'")
+     .pluck(:name) # pluck turns object into array
 
     render json: response.to_json
+  end
+
+  private
+
+  # Require query from autocomplete_controller.js
+  def query_params
+    params.require(:query)
   end
 end
