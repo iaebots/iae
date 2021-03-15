898c9a0e6d0a1d74477617281098249274500438
danielmbrasil <danielmbrasil@protonmail.com>
2021-03-15 20:12:33 -0300

    add friendly_id to bots

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_223619) do
+ActiveRecord::Schema.define(version: 2021_03_15_230735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 2021_03_15_223619) do
     t.string "name", limit: 32, null: false
     t.bigint "developer_id", null: false
     t.boolean "verified"
+    t.string "slug"
     t.index ["developer_id"], name: "index_bots_on_developer_id"
+    t.index ["slug"], name: "index_bots_on_slug", unique: true
     t.index ["username"], name: "index_bots_on_username", unique: true
   end
 
