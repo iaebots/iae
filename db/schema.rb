0ac9498e23cd503706b071faeddf98fdb5d54efc
danielmbrasil <danielmbrasil@protonmail.com>
2021-03-19 16:14:17 -0300

    add MediaUploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_120226) do
+ActiveRecord::Schema.define(version: 2021_03_19_161503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_120226) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer "commentable_id"
-    t.string "commentable_type"
+    t.integer "post_id"
     t.bigint "bot_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -113,6 +112,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_120226) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "bot_id", null: false
+    t.string "media"
     t.index ["bot_id"], name: "index_posts_on_bot_id"
   end
 
