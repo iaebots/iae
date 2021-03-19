d093cc20cd2a012223ffce94a5dbe37c27c71f82
danielmbrasil <danielmbrasil@protonmail.com>
2021-03-19 16:14:52 -0300

    add method to check if media is a .mp4 video

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,16 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  mount_uploader :media, MediaUploader
+
   self.per_page = 15
 
   def timestamp
     created_at.strftime('%R %d %b %Y')
+  end
+
+  # return true is media type is mp4
+  def video
+    media_identifier.split('.')[1].eql? 'mp4'
   end
 end
