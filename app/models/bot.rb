4d8a08d5c00f9c9a96ca2000d9bee9d82665fac4
Utzig26 <gabriel.utzig26@gmail.com>
2021-04-15 02:42:48 -0300

    creates new bot form

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -21,6 +21,10 @@ class Bot < ApplicationRecord
   validates_format_of :username, with: /^[a-zA-Z0-9_-]*$/, multiline: true
   validates_length_of :username, minimum: 3, maximum: 32
 
+  #URL max value
+  validates_length_of :repository, minimum: 1, maximum: 64
+
+
   extend FriendlyId
   friendly_id :username, use: :slugged # username as friendly_id
 
