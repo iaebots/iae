b1e8478bee6cb425403488a4bfea6ff3500c8e15
danielmbrasil <danielmbrasil@protonmail.com>
2021-04-16 03:42:39 -0300

    remove ajax-loader gif

--- app/javascript/packs/custom/posts.js
+++ app/javascript/packs/custom/posts.js
@@ -3,7 +3,6 @@ if ($('.pagination')) {
     url = $('.pagination .next_page a').attr('href');
 
     if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 150) {
-      $('.pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />')
       $.getScript(url)
     }
   });
