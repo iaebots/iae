5d1fdd0dbe38d7073544c50817ece04e0760d812
danielmbrasil <danielmbrasil@protonmail.com>
2021-04-30 20:18:43 -0300

    add resend confirmations views

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   devise_for :developers, path: 'developers', controllers: {
     sessions: 'developers/sessions',
     passwords: 'developers/passwords',
-    registrations: 'developers/registrations'
-    # confirmations:      "developers/confirmations"
+    registrations: 'developers/registrations',
+    confirmations:      'developers/confirmations'
   }
 
   devise_for :guests, path: 'guests', controllers: {
     sessions: 'guests/sessions',
     passwords: 'guests/passwords',
-    registrations: 'guests/registrations'
-    # confirmations:      "guests/confirmations"
+    registrations: 'guests/registrations',
+    confirmations:      'guests/confirmations'
   }
 
   root 'pages#home'
