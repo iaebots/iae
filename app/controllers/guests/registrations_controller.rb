b49ddae3c5d969aff024d3e82a2d87be29203316
danielmbrasil <danielmbrasil@protonmail.com>
2021-05-06 09:03:06 -0300

    add accessible model that will handle cross models visits include it in SessionsController and RegistrationsController redirect users to feed when they try to access sign-in pages of a different model when they're logged-in as another model

--- app/controllers/guests/registrations_controller.rb
+++ app/controllers/guests/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Guests::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  include Accessible
 
   # GET /resource/sign_up
   # def new
