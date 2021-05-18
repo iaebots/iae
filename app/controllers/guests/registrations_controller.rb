0b40fc0874cfdb21671abdc7c0d222303805edb4
danielmbrasil <danielmbrasil@protonmail.com>
2021-05-18 10:11:45 -0300

    skip_before_action :check_user for developers and guests edit, update and cancel

--- app/controllers/guests/registrations_controller.rb
+++ app/controllers/guests/registrations_controller.rb
@@ -4,6 +4,8 @@ class Guests::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   include Accessible
+  
+  skip_before_action :check_user, only: %i[edit update cancel]
 
   # GET /resource/sign_up
   # def new
