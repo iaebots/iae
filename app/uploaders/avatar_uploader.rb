ef6d6a786b1e4ef28cd82a4801d373db6b1a44c5
danielmbrasil <danielmbrasil@protonmail.com>
2021-05-19 21:03:37 -0300

    validate content_type of uploaded images

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,14 +4,16 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
 
+  before :process, :validate
+
   # Directory where uploaded files will be stored.
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded
-  def default_url(*args)
-    ActionController::Base.helpers.asset_path("fallback/" + ["default.png"].compact.join('_'))
+  def default_url(*_args)
+    ActionController::Base.helpers.asset_path('fallback/' + ['default.png'].compact.join('_'))
   end
 
   # All uploads will be resized to 400x400
@@ -24,11 +26,23 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Allowlist of extensions which are allowed to be uploaded.
   def extension_allowlist
-    %w(jpg jpeg png gif)
+    %w[jpg jpeg png gif]
+  end
+
+  def content_type_allowlist
+    [%r{image/}]
   end
 
   # Override the filename of the uploaded files
   def filename
     "#{model.username.to_s.underscore}.#{file.extension}" if original_filename.present?
+  end
+
+  # validate content type of image before processing it
+  def validate(file)
+    image = MiniMagick::Image.open(file.path)
+  rescue StandardError
+    raise CarrierWave::IntegrityError, I18n.translate(:"errors.messages.content_type_whitelist_error", content_type: content_type,
+                                                                                                       allowed_types: Array(content_type_allowlist).join(', '), default: :"errors.messages.content_type_allowlist_error")
   end
 end
--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -4,6 +4,8 @@ class CoverUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
 
+  before :process, :validate
+
   # Override the directory where uploaded files will be stored.
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -22,8 +24,20 @@ class CoverUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+  def content_type_allowlist
+    [%r{image/}]
+  end
+
   # Override the filename of the uploaded files:
   def filename
     "#{model.username.to_s.underscore}-cover.#{file.extension}" if original_filename
+  end
+
+  # validate content type of image before processing it
+  def validate(file)
+    image = MiniMagick::Image.open(file.path)
+  rescue StandardError
+    raise CarrierWave::IntegrityError, I18n.translate(:"errors.messages.content_type_whitelist_error", content_type: content_type,
+                                                                                                       allowed_types: Array(content_type_allowlist).join(', '), default: :"errors.messages.content_type_allowlist_error")
   end
 end
