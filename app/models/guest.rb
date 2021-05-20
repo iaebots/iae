65395ae3b798a4bd7a02b91a48083bb52492d679
danielmbrasil <danielmbrasil@protonmail.com>
2021-05-20 10:54:14 -0300

    scan code with brakeman change some regex and queries

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -3,25 +3,26 @@ class Guest < ApplicationRecord
          :recoverable, :rememberable, :validatable, authentication_keys: [:login]
 
   attr_writer :login
+
   acts_as_follower
-  
+
   has_many :likes, dependent: :destroy
 
   validate :validate_username, if: :username_changed?
 
   # regex to assure username doesn't have a @
-  validates_format_of :username, with: /^[a-zA-Z0-9_-]*$/, multiline: true
+  validates_format_of :username, with: /\A[a-zA-Z0-9_-]*\z/
   validates_length_of :username, minimum: 4, maximum: 32
   before_save :downcase_username
 
-  #validates to guidelines
-  validates :accept_terms, :acceptance => true 
+  # validates to guidelines
+  validates :accept_terms, acceptance: true
 
   # validates if password has at least 1 capital, at least 1 number and at least
   # one lower case. Min length 6, max length 64
-  validates_format_of :password, with: /^(?=.*[A-Z].*)(?=.*[0-9].*)(?=.*[a-z].*).{6,64}$/, 
-    multiline: true, message: 'must contain at least one capital, one lowercase and one number', if: :encrypted_password_changed?
-    
+  validates_format_of :password, with: /\A(?=.*[A-Z].*)(?=.*[0-9].*)(?=.*[a-z].*).{6,64}\z/,
+                                 message: 'must contain at least one capital, one lowercase and one number', if: :encrypted_password_changed?
+
   # remove all capitals from usernames
   def downcase_username
     username.downcase!
