8bbda330db00efdae070bdecf29afd979c296132
Utzig26 <gabriel.utzig26@gmail.com>
2021-05-24 19:00:32 -0300

    Double timeout time

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -186,7 +186,7 @@ Devise.setup do |config|
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
 
   # ==> Configuration for :timeoutable
-  config.timeout_in = 30.minutes
+  config.timeout_in = 60.minutes
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
