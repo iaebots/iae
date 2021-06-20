50fb6153d8d7af63a5ca0630d193694221985695
Daniel M Brasil <danielmbrasil@protonmail.com>
2021-06-19 22:07:47 -0300

    add helpers for posts and developers use helpers on views instead of local-time function

--- app/helpers/developers_helper.rb
+++ app/helpers/developers_helper.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
+# Helpers for Developers
 module DevelopersHelper
+  def created_at_formatted(created_at)
+    I18n.l(created_at.in_time_zone, format: :date)
+  end
 end
