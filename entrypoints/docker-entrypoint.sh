21b81dd062ea6c6c32b327186cf9aba5bee14826
Daniel M Brasil <danielmbrasil@protonmail.com>
2021-06-29 20:45:25 -0300

    add Dockerfile and docker-compose for development env add new config to database.yml

---
+++ entrypoints/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+unset BUNDLE_PATH
+unset BUNDLE_BIN
+
+set -e
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+bundle exec rails db:migrate
+bundle exec rails s -b 0.0.0.0
