04b9cb4d57bc8b0ad049a121ceee1bd1fd399ec2
Utzig26 <gabriel.utzig26@gmail.com>
2021-07-21 23:43:06 -0300

    Notice alert & Autohide navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,10 +34,22 @@ require('packs/custom/bootstrap-tagsinput')
 //func when doc is full loaded
 $(document).ready(function() {
   require("packs/custom/particles")
-  
   setTimeout(function() {
     $('#notice-wrapper').fadeOut("slow", function() {
       $(this).remove();
     })
-  }, 2500);
+  }, 10000);
+});
+
+$(document).ready(function() {
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 });
