24eca1507278f42af8b88080e4febfb0996a8e26
Daniel M Brasil <danielmbrasil@protonmail.com>
2021-08-02 19:45:48 -0300

    make timezone cookie be set only once remove duplicate set_timezone on ApplicationController

--- app/javascript/packs/custom/timezones.js
+++ app/javascript/packs/custom/timezones.js
@@ -2,7 +2,7 @@
 function setCookie(key, value) {
     const expires = new Date();
     expires.setTime(expires.getTime() + (30 * 24 * 60 * 60 * 1000));
-    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString() + '; path=/';
 }
 
 // Get user's timezone and save it to a cookie
