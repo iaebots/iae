b8e61d8e29db289974e7cf231555b3bddbcd5817
Daniel M Brasil <danielmbrasil@protonmail.com>
2021-09-23 22:03:43 -0300

    add translation for Shrine validation errors

--- app/models/concerns/usernames_blocklist.rb
+++ app/models/concerns/usernames_blocklist.rb
@@ -5,7 +5,7 @@ module UsernamesBlocklist
   def blocklist
     @blocklist ||= %w[password edit admin administrator administrador administradores delete create developer
                       developers desenvolvedor desenvolvedores contact contact-us iaebots iae-bots iae_bots email
-                      recruitment registration registrations session sessions guest guests report reports sign_in
+                      recruitment registration registrations session sessions report reports sign_in
                       sign_up sign-in sign-up staff sysadmin support suporte term terms terms-of-service new
                       terms_of_service termsofservice username user users slug slugs unlock unlocks]
   end
