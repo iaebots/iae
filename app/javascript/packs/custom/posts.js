81f2889a276ba62c79812c38d6d68494fad11028
Daniel M Brasil <danielmbrasil@protonmail.com>
2021-10-27 02:49:27 -0300

    update search and autocomplete input

--- app/javascript/packs/custom/posts.js
+++ app/javascript/packs/custom/posts.js
@@ -2,7 +2,7 @@ if ($('.pagination')) {
   $(window).scroll(function() {
     let url = $('.pagination .next_page a').attr('href');
 
-    if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 1500) {
+    if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 700) {
       $('.pagination').html('<div class="loader" width="70" height="70"/>')
       $.getScript(url)
     }
