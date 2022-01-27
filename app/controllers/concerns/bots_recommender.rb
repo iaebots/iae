700f6f4083a52b30d711f3e06f5f25e0150568f4
Daniel M Brasil <danielmbrasil@protonmail.com>
2022-01-27 14:31:15 -0300

    Remove predictor and explore page

--- app/controllers/concerns/bots_recommender.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class BotsRecommender
-  include Predictor::Base
-
-  input_matrix :tags, weight: 5.0
-end
--- config/initializers/predictor.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-Predictor.redis = Redis.new(url: ENV['PREDICTOR_REDIS'], driver: :hiredis)
